0a3c8039cd8773d2d8b52e4492408e845724937c
rootmeskid <39246539+rootmeskid@users.noreply.github.com>
2018-06-08 20:04:38 -0400

    Update pathbot.py

--- pathbot.py
+++ pathbot.py
@@ -4,17 +4,19 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 
+client = discord.Client()
+
 bot = commands.bot(command_prefix='.')
 
-@bot.event
+@client.event
 async def on_ready():
-print("The Cannons Are Ready To Fire!!")
-print("Not Yet Megaman 11")
+print('The Cannons Are Ready To Fire!!')
+print('Not Yet Megaman 11')
 
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def purge(context, number : int):	
 """Clear a specified number of messages in the chat"""	
 deleted = await bot.purge_from(context.message.channel, limit=number)	
 await bot.send_message(context.message.channel, 'Deleted {} message(s)'.format(len(deleted)))
 
-bot.run("NDU0NzcxMjAxMzM2ODAzMzQw.DfymaQ.6TpgZjXC83d7I0XrCz4sU6LQvTs")
+client.run("NDU0NzcxMjAxMzM2ODAzMzQw.DfymaQ.6TpgZjXC83d7I0XrCz4sU6LQvTs")
