962c5a6efdd8d4a52b6dd4fde18ac4518ede0953
rootmeskid <39246539+rootmeskid@users.noreply.github.com>
2018-06-08 20:24:17 -0400

    update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,12 @@ bot = commands.bot(command_prefix='.')
 
 @bot.command()
 async def test(ctx):
-    pass
+   await ctx.send(arg)
  
 @client.event
 async def on_ready():
 print('The Cannons Are Ready To Fire!!')
 print('Not Yet Megaman 11')
 
+bot.add_command(test)
 client.run('NDU0NzcxMjAxMzM2ODAzMzQw.DfymaQ.6TpgZjXC83d7I0XrCz4sU6LQvTs')
