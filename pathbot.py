12acbdfa68c1c807c783b96a6b2916c92c6a3819
rootmeskid <39246539+rootmeskid@users.noreply.github.com>
2018-06-08 20:14:56 -0400

    update pathbot.py

--- pathbot.py
+++ pathbot.py
@@ -13,10 +13,4 @@ async def on_ready():
 print('The Cannons Are Ready To Fire!!')
 print('Not Yet Megaman 11')
 
-@client.command(pass_context=True)
-async def purge(context, number : int):	
-"""Clear a specified number of messages in the chat"""	
-deleted = await bot.purge_from(context.message.channel, limit=number)	
-await bot.send_message(context.message.channel, 'Deleted {} message(s)'.format(len(deleted)))
-
-client.run("NDU0NzcxMjAxMzM2ODAzMzQw.DfymaQ.6TpgZjXC83d7I0XrCz4sU6LQvTs")
+client.run('NDU0NzcxMjAxMzM2ODAzMzQw.DfymaQ.6TpgZjXC83d7I0XrCz4sU6LQvTs')
